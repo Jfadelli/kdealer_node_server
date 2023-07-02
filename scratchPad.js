ff83af2a6c526f30665ae2fc301db3377c2e72b8
Jason <jfadelli@gmail.com>
2023-07-02 12:04:41 -0700

    test

---
+++ scratchPad.js
@@ -0,0 +1,8 @@
+let vehicleDetailsApi = "https://kdplusservices.kdealer.com/SALES/api/v1/VehicleMasterApi/VehicleMasterDetails"
+let vehicleDetailsApiPayload = {
+    vin: "5XXG44J83RG231730", //vin will be dynamic
+    dealerCode: "CA317"
+}
+
+let monroneyApi = "https://kdplusservices.kdealer.com/SALES/api/v1/VehicleMasterApi/MonroneyPDF"
+let monroneyApiPayload = { vin: "5XXG44J83RG231730" } //vin will be dynamic
